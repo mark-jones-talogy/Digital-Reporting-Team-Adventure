be20a82d351aa1af99b7a3ee9b4dfbe8ec923190
mark-jones-talogy <Mark.Jones@talogy.com>
2024-12-04 15:40:16 +0000

    Reorganise tests so they are in consistent a namespace

--- DigitalReportingTeamAdventureTests/Services/StateOptionsServiceTests.cs
+++ DigitalReportingTeamAdventureTests/Services/StateOptionsServiceTests.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DigitalReportingTeamAdventure.Services.Services
+namespace DigitalReportingTeamAdventureTests.Services
 {
     [TestClass()]
     public class StateOptionsServiceTests
--- DigitalReportingTeamAdventureTests/Services/StateServiceTests.cs
+++ DigitalReportingTeamAdventureTests/Services/StateServiceTests.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DigitalReportingTeamAdventure.Services.UnitTests
+namespace DigitalReportingTeamAdventureTests.Services
 {
     [TestClass()]
     public class StateServiceTests
